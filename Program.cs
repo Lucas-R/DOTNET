c3223001609027fe88faf0a2d38919b4f70fb6d4
Lucas Rodrigues <lucasrodriguesbezerra@outlook.com>
2025-09-24 17:46:03 -0300

    feat: add swagger

--- Program.cs
+++ Program.cs
@@ -5,19 +5,38 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(5000);
-    options.ListenAnyIP(5001, listenOptions => 
+    options.ListenAnyIP(5001, listenOptions =>
     {
         listenOptions.UseHttps();
     });
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddScoped<DatabaseContext>();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(custom =>
+{
+    custom.SwaggerDoc("v1", new() { Title = "Imobly API", Version = "v1" });
+});
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()){ app.MapOpenApi(); }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(); 
+}
 
 app.UseHttpsRedirection();
 app.MapControllers();
